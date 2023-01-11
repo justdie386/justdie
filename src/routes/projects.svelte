0038ee701104bd4f720517ba0a0cf2f4f8276e02
justdie386 <79466202+justdie386@users.noreply.github.com>
2023-01-11 14:12:22 -0500

    Update projects.svelte

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -18,8 +18,8 @@
         display: block;
         border-radius: 5px;
         outline: solid;
-        width: 450px;
-        margin: 16px;
+        width: 100%;
+        margin: 100%;
         padding: 2px 4px;
         text-align: center;
         font-family: JetBrains Mono, monospace;
