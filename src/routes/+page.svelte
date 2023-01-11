688c0fcb52e2d071c848b9f0d9053036f6817c13
justdie386 <79466202+justdie386@users.noreply.github.com>
2023-01-11 14:14:12 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,8 @@
 		padding: 10px;
 		margin-left: auto;
 		margin-right: auto;
+		min-height: 100%;
 		width: 100%;
-		height: 100%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
